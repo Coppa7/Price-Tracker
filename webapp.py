d2fbd3a6a18ea59b101a189e54921ddcab535eec
Coppa7 <gennydav2005@gmail.com>
2025-12-30 21:04:05 +0100

    Added database for bookmarks, updated main page css

--- webapp.py
+++ webapp.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, session, url_for, redirect, jsonify
 from datetime import timedelta
 from scraper.amazon_scraper import get_product_details
+import sqlite3
+import os
 
 app = Flask(__name__)
 app.secret_key = "ciao_mamma"
@@ -8,6 +10,41 @@ app.secret_key = "ciao_mamma"
 
 app.permanent_session_lifetime = timedelta(days=3650)
 #Cookies can get removed by the user, otherwise they're semi-permanent
+
+#We create a global database (all the bookmarked products shared between the users)
+folder = 'database_dir'
+path = os.path.join(folder, 'database.db')
+
+if not os.path.exists(folder):
+    os.makedirs(folder)
+
+connection = sqlite3.connect(path)
+
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS bookmarks (
+    asin TEXT PRIMARY KEY,
+    name TEXT,
+    price FLOAT,
+    discount TEXT,
+    img_src TEXT
+)
+''')
+
+#Database for graph data
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS price_history (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    asin TEXT,
+    price FLOAT,
+    date DATETIME,
+    FOREIGN KEY (asin) REFERENCES products (asin)
+)
+''')
+
+connection.commit()
+connection.close()
 
 @app.route("/")
 def home():
@@ -38,10 +75,29 @@ def query():
     
 @app.route("/bookmark", methods=["POST"])
 def bookmark_func():
+    connection = sqlite3.connect(path)
+    cursor = connection.cursor()
+    
     session.permanent = True #Permanent cookies
     MAX_BOOKMARKS = 4 #Limit of Bookmarks for a user 
     
     prod_details = request.get_json() #Gets product details from product.html
+    price_text = f"{prod_details.get('price_whole')}{prod_details.get('price_fraction')}"
+    price = float(price_text.replace(",", "."))
+    sql_query = '''
+    INSERT OR REPLACE INTO bookmarks (asin, name, price, discount, img_src)
+    VALUES(?, ?, ?, ?, ?)
+    '''
+    
+    cursor.execute(sql_query, (
+    prod_details.get("ASIN"), 
+    prod_details.get("name"), 
+    price, 
+    prod_details.get("discount"), 
+    prod_details.get("img_src")
+    ))
+    connection.commit()
+    connection.close()
     
     bookmarks_list = session.get("bookmarks_list", []) #Gets the bookmarks list from the session (or creates a new one)
     
@@ -54,7 +110,7 @@ def bookmark_func():
         "img_src": prod_details.get("img_src"),
         "ASIN": prod_details.get("ASIN"),
         "name": prod_details.get("name"),
-        "price": f"{prod_details.get('price_whole')}{prod_details.get('price_fraction')}",
+        "price": price_text,
         "discount": prod_details.get("discount")
     }
     
@@ -69,12 +125,24 @@ def bookmark_func():
 
 @app.route("/unbook", methods = ["POST"])
 def unbook_func():
+    connection = sqlite3.connect(path)
+    cursor = connection.cursor()
+
     bookmarks_list = session.get("bookmarks_list", [])
     ASIN_to_delete = request.get_json()
     session["bookmarks_list"] = [b for b in bookmarks_list if b.get("ASIN") != ASIN_to_delete]
     session.modified = True
     
+    sql_query = "DELETE FROM bookmarks WHERE asin = ?"
+    
+    cursor.execute(sql_query, (ASIN_to_delete))
+    
+    connection.commit()
+    connection.close()
+    
+    
     return "", 204    
+    
     
     
 @app.route("/error")
@@ -87,3 +155,4 @@ def error_page():
 
 if __name__ == '__main__': 
     app.run(debug=True)
+
