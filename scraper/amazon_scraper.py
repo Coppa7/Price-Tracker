24f44b80e80fe494311f9cd98c3d08972dcbda04
Coppa7 <gennydav2005@gmail.com>
2026-02-23 21:20:56 +0100

    Added additional error handling case

--- scraper/amazon_scraper.py
+++ scraper/amazon_scraper.py
@@ -6,8 +6,8 @@ def get_product_details(url):
     url = url.strip()
     
     # Fix for urls that were copied wrong
-    if not url.startswith(("http://", ("https://"))):
-        url = url + "https://"
+    if not url.startswith(("http://", "https://")):
+        url = "https://" + url
     
     '''
     Uses scraping to find the price and discount (if there is one) of an item on amazon.it.
@@ -44,49 +44,56 @@ def get_product_details(url):
     soup = BeautifulSoup(html, "html.parser")
 
     main_container = soup.find("div", id = "centerCol")
+    if not main_container:
+        main_container = soup.find("div", id = "rightCol")
+        if not main_container:
+            html_lower = html.lower()
+            if "captcha" in html_lower or "robot check" in html_lower:
+                print(f"Scraper diagnostic: captcha/robot check detected. final_url={response.url} status={response.status_code}")
+                return "-3", "Error", "Error", "Error", "", "Error", "N/A"
+            print(f"Scraper diagnostic: main container not found. final_url={response.url} status={response.status_code}")
+            return "-2", "Error", "Error", "Error", "", "Error", "N/A"
+            
     img_container = soup.find("div", id = "leftCol")
     
-    if main_container:
-        #Find the ASIN of the amazon product (unique id) from the url 
-        # .../name/dp/ASIN/...
-        #We find the ASIN by splitting the url twice
-        
-        ASIN = ((url.split("/dp/"))[1].split("/"))[0]
-        
-        price_whole = main_container.find("span", class_="a-price-whole")
-        price_fraction = main_container.find("span", class_="a-price-fraction")
-        name = main_container.find("span", id = "productTitle")
+    #Find the ASIN of the amazon product (unique id) from the url 
+    # .../name/dp/ASIN/...
+    #We find the ASIN by splitting the url twice
+    
+    ASIN = ((url.split("/dp/"))[1].split("/"))[0]
+    
+    price_whole = main_container.find("span", class_="a-price-whole")
+    price_fraction = main_container.find("span", class_="a-price-fraction")
+    name = main_container.find("span", id = "productTitle")
 
-        discount = main_container.find("span", class_="savingsPercentage")
-        
-        
-        if ASIN and price_whole and price_fraction and name:
-            price_whole = price_whole.get_text(strip=True)
-            price_fraction = price_fraction.get_text(strip=True)
-            name = name.get_text(strip=True)
-        else:
-            ASIN  = "Product not found"
-            price_whole = "Price not found"
-            price_fraction = ""
-            name = "Name not found"
-        if discount:
-            discount = discount.get_text(strip=True)
-        else:
-            discount = "Discount not found"
-            
-        #Searching for image
-        
-        if img_container:
-            img_tag = soup.find("img", id = "landingImage")
-            if img_tag:
-                img_url = img_tag['src']
-                return "0", ASIN, name, price_whole, price_fraction, discount, img_url
-            
-        
-        return "1", ASIN, name, price_whole, price_fraction, discount, "N/A"
+    discount = main_container.find("span", class_="savingsPercentage")
+    
+    
+    if ASIN and price_whole and price_fraction and name:
+        price_whole = price_whole.get_text(strip=True)
+        price_fraction = price_fraction.get_text(strip=True)
+        name = name.get_text(strip=True)
     else:
-        return "-2", "Error", "Error", "Error", "", "Error", "N/A"
+        ASIN  = "Product not found"
+        price_whole = "Price not found"
+        price_fraction = ""
+        name = "Name not found"
+    if discount:
+        discount = discount.get_text(strip=True)
+    else:
+        discount = "Discount not found"
         
+    #Searching for image
+    
+    if img_container:
+        img_tag = soup.find("img", id = "landingImage")
+        if img_tag:
+            img_url = img_tag['src']
+            return "0", ASIN, name, price_whole, price_fraction, discount, img_url
+        
+    
+    return "1", ASIN, name, price_whole, price_fraction, discount, "N/A"
+    
     
     
     
